a15f892131d80d85d5639f25eea8d9891a08f0a7
davincism <novelistsm@gmail.com>
2019-12-24 01:10:08 +0900

    모든 프로젝트 추가

---
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loadData()
+
+        resultButton.setOnClickListener {
+            if (TextUtils.isEmpty(weightEditText.text) ||
+                TextUtils.isEmpty(heightEditText.text)) {
+                toast("값을 입력해 주세요 please")
+                return@setOnClickListener
+            }
+
+            saveData(heightEditText.text.toString().toInt(),
+                weightEditText.text.toString().toInt())
+
+            startActivity<ResultActivity>(
+                "weight" to weightEditText.text.toString(),
+                "height" to heightEditText.text.toString()
+            )
+        }
+    }
+
+    private fun saveData(height: Int, weight: Int) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+
+        editor.putInt("KEY_HEIGHT", height)
+            .putInt("KEY_WEIGHT", weight)
+            .apply()
+    }
+
+    private fun loadData() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val height = pref.getInt("HEY_HEIGHT", 0)
+        val weight = pref.getInt("HEY_WEIGHT", 0)
+
+        if (height != 0 && weight != 0) {
+            heightEditText.setText(height.toString())
+            weightEditText.setText(weight.toString())
+        }
+    }
+}
