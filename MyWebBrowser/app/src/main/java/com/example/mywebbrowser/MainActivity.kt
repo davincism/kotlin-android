ba8cebfa813cff7fab9f16f4c98a3f16009654d6
davincism <novelistsm@gmail.com>
2019-12-26 14:40:02 +0900

    ADD: browser

--- MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.mywebbrowser
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.browse
+import org.jetbrains.anko.email
+import org.jetbrains.anko.sendSMS
+import org.jetbrains.anko.share
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,6 +42,8 @@ class MainActivity : AppCompatActivity() {
                 false
             }
         }
+
+        registerForContextMenu(webView)
     }
 
     override fun onBackPressed() {
@@ -41,6 +52,74 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_google, R.id.action_home -> {
+                webView.loadUrl("https://www.google.com")
+                return true
+            }
+            R.id.action_naver -> {
+                webView.loadUrl("https://www.naver.com")
+                return true
+            }
+            R.id.action_daum -> {
+                webView.loadUrl("https://www.daum.net")
+                return true
+            }
+            R.id.action_call -> {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:031-123-4567")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.action_send_text -> {
+                /*val intent = Intent(Intent.ACTION_SEND)
+                intent.apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, "보낼 문자열")
+                    var chooser = Intent.createChooser(intent, null)
+                    if (intent.resolveActivity(packageManager) != null) startActivity(chooser)
+                }*/
+                sendSMS("031-123-4567", webView.url)
+                return true
+            }
+            R.id.action_email -> {
+                /*val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse("http://www.example.com")
+                if (intent.resolveActivity(packageManager) != null) startActivity(intent)*/
+                email("test@example.com", "좋은 사이트", webView.url)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_share -> {
+                share(webView.url)
+                return true
+            }
+            R.id.action_browser -> {
+                browse(webView.url)
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 }
