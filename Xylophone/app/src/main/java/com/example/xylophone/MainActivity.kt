41d85258a1ba4161e619e28773ba33af2a8ca44c
davincism <novelistsm@gmail.com>
2020-01-02 03:33:48 +0900

    WIP: todo

---
+++ Xylophone/app/src/main/java/com/example/xylophone/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.xylophone
+
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val soundPool = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        SoundPool.Builder().setMaxStreams(8).build()
+    } else {
+        SoundPool(8, AudioManager.STREAM_MUSIC, 0)
+    }
+
+    private val sounds = listOf(
+        Pair(R.id.do1, R.raw.do1),
+        Pair(R.id.re, R.raw.re),
+        Pair(R.id.mi, R.raw.mi),
+        Pair(R.id.fa, R.raw.fa),
+        Pair(R.id.sol, R.raw.sol),
+        Pair(R.id.la, R.raw.la),
+        Pair(R.id.si, R.raw.si),
+        Pair(R.id.do2, R.raw.do2)
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE 가로 방법 1
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*비디오, 오디오 재생 시
+        val mediaPlayer = MediaPlayer.create(this, R.raw.do1)
+        button.setOnClickListener{ mediaPlayer.start() }
+        mediaPlayer.release() 사용 끝나면 릴리즈*/
+
+        sounds.forEach { tune(it) }
+    }
+
+    private fun tune(pitch: Pair<Int, Int>) {
+        val soundId = soundPool.load(this, pitch.second, 1)
+        findViewById<TextView>(pitch.first).setOnClickListener {
+            soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
+    }
+}
