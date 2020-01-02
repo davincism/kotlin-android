41d377b50eb1c4529bcbfee81694dc80e407f736
davincism <novelistsm@gmail.com>
2020-01-02 16:01:38 +0900

    ADD: Todo

--- TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -5,20 +5,33 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import io.realm.Realm
+import io.realm.Sort
+import io.realm.kotlin.where
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
+    val realm = Realm.getDefaultInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val realmResult = realm.where<Todo>().findAll().sort("date", Sort.DESCENDING)
+        val adapter = TodoListAdapter(realmResult)
+        listView.adapter = adapter
+
+        realmResult.addChangeListener { _-> adapter.notifyDataSetChanged() } //데이터가 변겨되면 어댑터에 적용
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            startActivity<EditActivity>("id" to id) //할 일 수정
         }
+
+        fab.setOnClickListener { startActivity<EditActivity>() } //새 할 일 추가
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -35,5 +48,10 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 }
